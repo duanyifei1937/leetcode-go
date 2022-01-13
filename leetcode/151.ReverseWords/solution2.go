31b0ea5650f010352ade22649e2f9b00d32fcb48
duanyifei <duanyifei1937@gmail.com>
2022-01-13 10:11:54 +0800

    juan

--- leetcode/151.ReverseWords/solution2.go
+++ leetcode/151.ReverseWords/solution2.go
@@ -2,20 +2,6 @@ package leetcode
 
 func reverseWords2(s string) string {
 	ss := []byte(s)
-	
-	// //第一次翻转
-	// reverString(ss)
-
-	// // 第二次按空格翻转
-	// var start, end int
-	// for i :=0; i<len(ss); i++{
-	// 	if ss[i] == ' ' {
-	// 		end = i
-	// 		reverString(ss[start:end])
-	// 		start = i + 1
-	// 	}
-	// }
-	// return string(ss)
 
 	slow, fast := 0, 0
 	// 删除头部空格
@@ -40,12 +26,23 @@ func reverseWords2(s string) string {
 	}
 
 
+	// 全部翻转
+	reverse2(&ss, 0, len(ss) -1)
 
+	// 单词翻转
+	i := 0
+	for i < len(ss) {
+		j := i
+		for ; j < len(ss) && ss[j] != ' '; j++ {
+		}
+		reverse2(&ss, i, j-1)
+		i = j
+		i++
+	}
+	return string(ss)
 }
 
-
-
-func reverse(b *[]byte, left, right int) {
+func reverse2(b *[]byte, left, right int) {
 	for left < right {
 		(*b)[right], (*b)[left] = (*b)[left], (*b)[right]
 		left++
--- leetcode/151.ReverseWords/solution_test.go
+++ leetcode/151.ReverseWords/solution_test.go
@@ -5,3 +5,8 @@ import "testing"
 func TestReverseWords(t *testing.T) {
 	t.Log(reverseWords(" ab  c hello duan world"))
 }
+
+
+func TestReverseWords2(t *testing.T) {
+	t.Log(reverseWords2("  ab  c hello duan    world   "))
+}
